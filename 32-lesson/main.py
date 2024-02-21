2397bd25e15fd51db0d48bd3fa15e89df5b9454c
davronkhandamov <xamdamovdavron6@gmail.com>
2024-02-21 20:36:53 +0500

    send email with smtp

---
+++ 32-lesson/main.py
@@ -0,0 +1,14 @@
+import smtplib
+
+my_email = "xamdamovdavron6@gmail.com"
+password = "grpj idyd sjag gnam"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="mynameispro@icloud.com",
+        msg="Subject:Hello bro what's up\n\nThis is body of my email",
+    )
