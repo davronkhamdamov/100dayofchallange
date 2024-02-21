a6e3751ace71bbc30b6d8b854dbbca89c41ae1ef
davronkhandamov <xamdamovdavron6@gmail.com>
2024-02-21 21:45:05 +0500

    send random quote to email

---
+++ 32-lesson/date.py
@@ -0,0 +1,18 @@
+import smtplib
+from datetime import datetime
+import random
+
+my_email = "xamdamovdavron6@gmail.com"
+password = "grpj idyd sjag gnam"
+
+
+with open("32-lesson/quote.txt", "r") as quotes:
+    random_quote = random.choice(quotes.readlines())
+    with smtplib.SMTP("smtp.gmail.com") as conn:
+        conn.starttls()
+        conn.login(user=my_email, password=password)
+        conn.sendmail(
+            from_addr=my_email,
+            to_addrs="davronx036@gmail.com",
+            msg=f"Subject: Smtp mail send mail test\n\n{random_quote}",
+        )
